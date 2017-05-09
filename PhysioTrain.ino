c9daee7cd6036fab5826ddb3f19c3c7669dca041
Andreas Bachmann <bachi@te-clan.ch>
2017-05-09 11:05:12 +0200

    PES4: communication between the two IMUs and the MCU

--- PhysioTrain.ino
+++ PhysioTrain.ino
@@ -4,57 +4,86 @@
  * 07.04.2017
  */
 
+#include "wiring_private.h" // pinPeripheral() function
 #include "config.h"
 
 #include "PhysioTrainLib.h"
 
-CLI  cli;
-IMU* imu;
+CLI             cli;
+IMU             imu(IMU_SELECT_INTERNAL);
+IMU             muxImu(IMU_SELECT_EXTERNAL);
+KinematicModel  model;
 
-const int calibSize = 10;
-EulerAngle angle[calibSize];
-EulerAngle  calib;
-bool isCalib = false;
+TwoWire muxWire(&sercom1, 11, 13);
+
 int i = 0;
 int u = 0;
+int v = 0;
+
+String q1;
+String q2;
+
 
 void setup() {
+    Serial.begin(SERIAL_BAUD_RATE);
     cli.begin();
+
+    Wire.begin();
+    muxWire.begin();        // join i2c bus (address optional for master)
+    pinPeripheral(11, PIO_SERCOM);
+    pinPeripheral(13, PIO_SERCOM);
+    
+    delay(5000);
+    Serial.println("Hallo");
     
     // Set up serial log port
-    imu = IMU::instance();
-    imu->begin();
+    imu.begin();
+    muxImu.begin();
+    Serial.println("Hallo");
 
+    //model.begin(&Wire, &imu);
+    Serial.println("Hallo");
 }
-
 void loop() {
+    Position p(1.0f, 2.0f, 3.0f);
+    
     //Serial.println("loop");
     cli.update();
-    if (imu->update()) {
+    if (imu.update()) {
         u++;
         if (u % 20 == 0) {
-            Acceleration a = imu->getAcceleration();
-            //EulerAngle e = imu->getEulerAngle();
-            Quaternion q = imu->getQuaternion();
-            
-            String str = "";
-            
-            str += String(a.getX(), 4) + ", ";
-            str += String(a.getY(), 4) + ", ";
-            str += String(a.getZ(), 4) + ", ";
+            Quaternion q = imu.getQuaternion();
 
-            /*
-            str += String(e.getRoll(),  4) + ", ";
-            str += String(e.getPitch(), 4) + ", ";
-            str += String(e.getYaw(),   4) + ", ";
-            */
+            //String q1;
+            q1   = String("[ ");
+            q1  += String(q.getW(), 4) + ", ";
+            q1  += String(q.getX(), 4) + ", ";
+            q1  += String(q.getY(), 4) + ", ";
+            q1  += String(q.getZ(), 4);
+            q1  += String(" ]");
             
-            str += String(q.getW(), 4) + ", ";
-            str += String(q.getX(), 4) + ", ";
-            str += String(q.getY(), 4) + ", ";
-            str += String(q.getZ(), 4) + "\r\n";
-            
-            Serial.print(str);
+            //Serial.print(q1);
         }
+    }
+
+    if (muxImu.update()) {
+        v++;
+        if (v % 20 == 0) {
+            Quaternion q = muxImu.getQuaternion();
+
+            q2   = String("[ ");
+            q2  += String(q.getW(), 4) + ", ";
+            q2  += String(q.getX(), 4) + ", ";
+            q2  += String(q.getY(), 4) + ", ";
+            q2  += String(q.getZ(), 4);
+            q2  += String(" ]");
+            
+        }
+    }
+
+    //String line = q1;
+    if (u % 20 == 0) {
+      String line = q1 + " " + q2 + "\r\n";
+      Serial.print(line);
     }
 }
